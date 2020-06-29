39d845e3b827ea27d8276c11d8e48517c55177c8
panxman <nentzos.pan@gmail.com>
2020-06-29 16:58:19 +1000

    Git Init

---
+++ src/app.js
@@ -0,0 +1,32 @@
+// Third Party
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+// Components
+import AppRouter from "./routers/AppRouter";
+// Store
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
+// Styles
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: "Water bill", amount: 3000, createdAt: 100 }));
+store.dispatch(addExpense({ description: "Gas bill", amount: 5000, createdAt: 200 }));
+store.dispatch(addExpense({ description: "Rent", amount: 45000, createdAt: 300 }));
+store.dispatch(addExpense({ description: "Phone bill", amount: 4000, createdAt: 400}));
+
+const jsx = (
+    <div>
+        <Provider store={store}>
+            <AppRouter />
+        </Provider>
+    </div>
+);
+
+/* Render */
+ReactDOM.render(jsx, document.querySelector("#app"));
---
+++ src/playground/hoc.js
@@ -0,0 +1,44 @@
+// Higher Order Component (HOC) - A component that renders another component
+// Reuse code
+// Render hijacking
+// Prop manipulation
+// Abstract state
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Info = (props) => (
+  <div>
+    <h1>Info</h1>
+    <p>The info is: {props.info}</p>
+  </div>
+);
+
+const withAdminWarning = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      {props.isAdmin && <p>This is private info, please do not share.</p>}
+      <WrappedComponent {...props} />
+    </div>
+  );
+};
+
+const requireAuthentication = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      {props.isAuthenticated ? (
+        <WrappedComponent {...props} />
+      ) : (
+        <p>Please login first.</p>
+      )}
+    </div>
+  );
+};
+
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuthentication(Info);
+
+// ReactDOM.render(<AdminInfo isAdmin={true} info="There are some details." />, document.getElementById("app"));
+ReactDOM.render(
+  <AuthInfo isAuthenticated={false} info="There are some details." />,
+  document.getElementById("app")
+);
