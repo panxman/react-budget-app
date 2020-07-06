4f4aa4c73ddd3e4b89a7428e9851897f607e7751
panxman <nentzos.pan@gmail.com>
2020-07-06 15:19:49 +1000

    Setting up Enzyme with Jest

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,6 @@ const Header = () => (
     <NavLink to="/create" activeClassName="is-active">
       Create
     </NavLink>
-    <NavLink to="/help" activeClassName="is-active">
-      Help
-    </NavLink>
   </header>
 );
 
