49a41dda474b21b55848817399da0c94330392dc
panxman <nentzos.pan@gmail.com>
2020-07-12 16:43:14 +1000

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,50 @@
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
+module.exports = (env) => {
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
+
+  return {
     entry: "./src/app.js",
     output: {
-        path: path.join(__dirname, "public"),
-        filename: "bundle.js"
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
     },
-    mode: "development",
+    mode: isProduction ? "production" : "development",
     module: {
-        rules: [{
-            loader: "babel-loader",
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
             use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader',
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
             ],
-        }]
+          }),
+        },
+      ],
     },
-    devtool: "cheap-module-eval-source-map",
+    plugins: [CSSExtract],
+    devtool: isProduction ? "source-map" : "inline-source-map",
     devServer: {
-        contentBase: path.join(__dirname, "public"),
-        historyApiFallback: true,
-    }
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+    },
+  };
 };
